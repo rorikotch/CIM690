d97b821fc5e8bbf08b558071c02f20207e24ed90
Rori Kotch <r.kotch1@umiami.edu>
2016-11-01 15:05:19 -0400

    added some functions

--- demo jquery/js/main.js
+++ demo jquery/js/main.js
@@ -1,1 +1,7 @@
-$("#joe").fadeOut(1000);
+//$("#joe").fadeOut(1000);
+$("#bob").click(function () {
+    $("#joe").fadeOut(1000);
+})
+$("#jane").click(function () {
+    $("#joe").fadeIn (1000);
+})
